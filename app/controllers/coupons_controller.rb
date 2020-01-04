dc2be907621a1eda114b3225c65e52e6f0963d4f
LaNice Powell <lanicemp@aol.com>
2020-01-04 18:42:13 -0500

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,14 +4,23 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all 
     #binding.pry
   end
+
   def show 
+    @coupon = Coupon.find(params[:id])
   end 
 
   def new 
+    @coupon = Coupon.new
   end 
 
   def create 
-    Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
-    redirect_to coupons_path
+    Coupon.create(coupon: params[:coupon_code], coupon: params[:store])
+    redirect_to coupon_path(@coupon)
   end 
 end
+
+#strong 
+#private 
+#def coupons_params 
+# params.require(:coupon).permit(:coupon_code, :store)
+#this is a way to sanitize the params and need to be put in a priviate
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191205171815) do
+ActiveRecord::Schema.define(version: 20191206004228) do
 
   create_table "coupons", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "coupon_code"
+    t.string   "store"
   end
 
 end
